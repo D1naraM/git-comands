dec5ef1d7307857cbe5cd853fb3c6e6a49a76a6a
dmosova <dmosova>
2023-07-23 20:52:23 +0400

    add ya-test

--- src/test/java/Test_example/YaTest.java
+++ src/test/java/Test_example/YaTest.java
@@ -1,10 +1,11 @@
 package Test_example;
 
+import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 
 public class YaTest {
     @Test
     void yaTest() {
-
+        Selenide.open("https://ya.ru");
     }
 }
